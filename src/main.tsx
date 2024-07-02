3bc293cadb189b42d41541a635f4957962c83b3f
강동우 <spripe13@naver.com>
2024-07-02 16:05:45 +0900

    Habit

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import { UserProvider } from './components/user-context.tsx';
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <UserProvider>
       <App />
-    </UserProvider>
   </React.StrictMode>,
 );
